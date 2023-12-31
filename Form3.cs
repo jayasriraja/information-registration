42b0e0ad29e629da648a6e9430457931c4b48058
Jayasri G <148430775+jayasriraja@users.noreply.github.com>
2023-12-31 20:06:10 +0530

    Form3.cs

--- Form3.cs
+++ Form3.cs
@@ -17,15 +17,17 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+        //this is a search page
+        // connection line to the database
         MySqlConnection con = new MySqlConnection("Server=localhost;Database=project1;Uid=root;Pwd=12345;");
 
         private void Form3_Load(object sender, EventArgs e)
         {
+            // this is used to show a saved and alive information from the connected database
                 {
                 con.Open();
                 MySqlDataAdapter cmd = new MySqlDataAdapter("select sno as Serial_No,idno as Id_No,name as Name,contactno as Contact_No from information where status='a'", con);
                 DataSet dd = new DataSet();
-
                 cmd.Fill(dd);
                 dataGridView1.DataSource = dd.Tables[0];
                 con.Close();
@@ -34,6 +36,7 @@ namespace WindowsFormsApplication1
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // this button is a search bar in a form and it is using a contact number from informaiton..
             con.Open();
             MySqlDataAdapter cmd = new MySqlDataAdapter("select sno as Serial_No,idno as ID_No,name as Name,contactno as Contact_No from project1.information where contactno like'" + textBox1.Text + "%'", con);
             DataSet dd = new DataSet();
@@ -44,6 +47,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // this button is a search bar in a form and it is using a name from informaiton..
             con.Open();
             MySqlDataAdapter cmd = new MySqlDataAdapter("select sno as Serial_No,idno as ID_No,name as Name,contactno as Contact_No from project1.information where name like'" + textBox1.Text + "%'", con);
             DataSet dd = new DataSet();
